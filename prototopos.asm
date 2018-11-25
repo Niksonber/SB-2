f992491a1b8639e801c93bbee2595333ba264cb6
nikson <nik.son18@hotmail.com>
2018-11-25 02:22:09 -0200

    INPUT e C_INPUT

--- prototopos.asm
+++ prototopos.asm
@@ -1,10 +1,64 @@
 %macro sum1	1
-	add byte [%1], 1
+    add byte [%1], 1
 %endmacro
 
 %macro return 1
+    mov EAX, 1
+    mov EBX, %1 
+    int 80h
+%endmacro
+
+%macro mult 1
+    mov ebx, [%1]
+    mov edx, eax
+    imul ebx
+    cmp eax,0
+    jl low
+gre:     
+    cmp edx, 0x00000000
+    je contmul
+    jmp over
+low:
+    cmp edx, 0xffffffff
+    je contmul
+over:
+    S_OUTPUT 1, o, 8
+    return 1
+contmul:
+%endmacro
+
+%macro jmpn 1
+    cmp EAX,0
+    jl %1
+%endmacro
+
+%macro jmpp 1
+    cmp EAX,0
+    jg %1
+%endmacro
+
+%macro jmpz 1
+    cmp EAX,0
+    jz %1
+%endmacro
+
+%macro copy 2
+    mov EBX, [%1]
+    mov [%2], EBX
+%endmacro
+
+%macro load 1
+	mov EAX, [%1]
+%endmacro
+
+%macro store 1
+	mov [%1], EAX
+%endmacro
+
+
+%macro stop 0
 	mov EAX, 1
-	mov EBX, %0 
+	mov EBX, 0 
 	int 80h
 %endmacro
 
@@ -42,51 +96,155 @@ co:
 %endmacro
 
 %macro atoi 3
-	mov ECX, 0
-	mov EAX, 0
-	mov EBX, 10
-lp:	cmp ECX, %3 
-	jae cont
-	mul EBX
-	add AL, [%2 + ECX]
-	adc AH, 0
-	sub EAX, 0x30
-	inc ECX
-	jmp lp
+    mov ECX, 0
+    mov EAX, 0
+    mov EBX, 10
+lp: cmp ECX, %3 
+    jae cont
+    mul EBX
+    add AL, [%2 + ECX]
+    adc AH, 0
+    sub EAX, 0x30
+    inc ECX
+    jmp lp
 cont:
-	mov dword [%1], EAX
+    mov dword [%1], EAX
 %endmacro
 
 %macro itoa 2
-	mov ECX, 0
-	mov EAX, [%2]
-	mov EBX, 10
-l:	cdq
-	div EBX
-	add EDX, 0x30
-	mov byte [%1 + ECX], DL
-	inc ECX
-;	sub EDX, EDX
-	cmp EAX, 0
-	jne l
-;	inc ECX
-	dec ECX
-	swap %1, ECX
-;	mov byte [%2 + ECX], 0
+    enter 0,0
+    pusha
+    mov ECX, 0
+    mov EAX, [%2]
+    mov EBX, 10
+    cmp EAX, 0
+    jge l
+    neg EAX
+l:  cdq
+    div EBX
+    add EDX, 0x30
+    mov byte [%1 + ECX], DL
+    inc ECX
+    ;sub EDX, EDX
+    cmp EAX, 0
+    jne l
+    ;inc ECX
+    cmp word [%2], 0
+    jge s
+    mov byte [%1 + ECX ], '-'
+    inc ECX
+s:  mov EAX, ECX
+    dec ECX
+    swap %1, ECX
+    ;S_OUTPUT 1, %1, ECX
+    ;mov byte [%2 + ECX], 0
+%endmacro
+%macro ler 2
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, [%1]
+    mov edx, %2
+    int 80h
 %endmacro
 
+%macro escreve 2
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, [%1]
+    mov edx, %2
+    int 80h
+%endmacro
+
+C_INPUT: ; INPUT(* char) 
+    enter 0,0
+    push eax
+    push ebx
+    push ecx
+    push edx
+    ler ebp+8, 1 
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+    leave
+    ret
+
+INPUT: ; INPUT(* int) 
+    enter 6,0
+    push eax
+    push ebx
+    push ecx
+    push edx
+    mov dword [ebp-6], 0
+    mov byte [ebp-2], 0
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, ebp
+    sub ecx, 1
+    mov edx, 1
+    int 80h
+    cmp byte [ebp-1], '-'
+    jne pli
+    mov byte [ebp-2], '-'
+loopinput:
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, ebp
+    sub ecx, 1
+    mov edx, 1
+    int 80h
+pli:
+    mov esi, [ebp-6]
+    cmp byte [ebp-1], 0xa   ;confere se eh fim de linha
+    je fiminput
+    cmp byte [ebp-1], 0x30  ;verifica se eh um numero valido
+    jb inv
+    cmp byte [ebp-1], 0x39  ;verifica se eh um numero valido
+    ja inv
+    mov EAX, 10
+    mul dword [ebp-6]
+    add byte AL, [ebp-1]
+    adc AH, 0
+    sub EAX, 0x30
+    mov dword [ebp-6], eax
+    jmp loopinput
+inv: 
+
+fiminput:
+    cmp byte [ebp-2], '-'
+    jne fimfiminput
+    neg dword [ebp-6]
+fimfiminput:
+    mov eax, [ebp-6]
+    mov ebx, [ebp+8]
+    mov dword [ebx], eax
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+    leave
+ret
 
 section .text
 
 global _start
 _start:
-	atoi n, a, TAMa 
+    mov ebp, esp; for correct debugging
+    push n
+    call INPUT
+;	atoi n, a, TAMa 
 ;	sub dword [n], 12
-	itoa b, n
-	S_OUTPUT 1, b, TAMa
-	sum1 a
+;        mov eax, 1023  
+ ;       mult n2
+ ;       mov [n],eax
+        itoa b, n
+        S_OUTPUT 1, b, 10
+       ; S_OUTPUT 1, b, TAMa
+        sum1 a
+        mov eax, 10
+      ;  mult n2
 
-	return 0
+	stop
 
 
 section .data
@@ -94,9 +252,11 @@ section .data
    ;      4294967295
 	TAMa EQU $-a
 	cd: db "isso eh um teste"
-	n2: dd 1234
+        o:  db "overflow"
+	n2: dd 10
 ;	b: db "0"
 	k: dd 329496712
 section .bss
 	n: resd	1
 	b: resb 100
+
