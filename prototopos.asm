0032b8c1989c76f64c7d631d5e19423e7ad87bf2
nikson <nik.son18@hotmail.com>
2018-11-25 04:11:59 -0200

    OUTPUT e C_OUTPUT

--- prototopos.asm
+++ prototopos.asm
@@ -1,4 +1,12 @@
-%macro sum1	1
+%macro endl 0
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, endlineeee
+    mov edx, 1
+    int 80h
+%endmacro
+
+%macro sum1 1
     add byte [%1], 1
 %endmacro
 
@@ -7,6 +15,8 @@
     mov EBX, %1 
     int 80h
 %endmacro
+
+
 
 %macro mult 1
     mov ebx, [%1]
@@ -48,52 +58,55 @@ contmul:
 %endmacro
 
 %macro load 1
-	mov EAX, [%1]
+    mov EAX, [%1]
 %endmacro
 
 %macro store 1
-	mov [%1], EAX
+    mov [%1], EAX
 %endmacro
 
 
 %macro stop 0
-	mov EAX, 1
-	mov EBX, 0 
-	int 80h
+    mov EAX, 1
+    mov EBX, 0 
+    int 80h
 %endmacro
 
 %macro S_INPUT 3
-	mov EAX, 3
-	mov EBX, %1
-	mov ECX, %2
-	mov EDX, %3
-	int 80h
+    mov EAX, 3
+    mov EBX, %1
+    mov ECX, %2
+    mov EDX, %3
+    int 80h
 %endmacro
 
 %macro S_OUTPUT 3
-	mov EAX, 4
-	mov EBX, %1
-	mov ECX, %2
-	mov EDX, %3
-	int 80h
+    mov EAX, 4
+    mov EBX, %1
+    mov ECX, %2
+    mov EDX, %3
+    int 80h
 %endmacro
 
-%macro swap 2
-	mov EAX, %1
-	mov ECX, %2
-	add ECX, %1
-l1:	cmp EAX, ECX
-	jae co
-	mov BL, [EAX]
-	mov DL, [ECX]
-	mov [EAX], DL
-	mov [ECX], BL
-	inc EAX
-	dec ECX
-	jmp l1
+swap:   
+     enter 0, 0
+     pusha
+    mov EAX, [ebp+12]
+    mov ECX, [ebp+8]
+    add ECX, eax
+l1: cmp EAX, ECX
+    jae co
+    mov BL, [EAX]
+    mov DL, [ECX]
+    mov [EAX], DL
+    mov [ECX], BL
+    inc EAX
+    dec ECX
+    jmp l1
 co:
-	
-%endmacro
+    popa
+    leave   
+ret 8
 
 %macro atoi 3
     mov ECX, 0
@@ -111,34 +124,7 @@ cont:
     mov dword [%1], EAX
 %endmacro
 
-%macro itoa 2
-    enter 0,0
-    pusha
-    mov ECX, 0
-    mov EAX, [%2]
-    mov EBX, 10
-    cmp EAX, 0
-    jge l
-    neg EAX
-l:  cdq
-    div EBX
-    add EDX, 0x30
-    mov byte [%1 + ECX], DL
-    inc ECX
-    ;sub EDX, EDX
-    cmp EAX, 0
-    jne l
-    ;inc ECX
-    cmp word [%2], 0
-    jge s
-    mov byte [%1 + ECX ], '-'
-    inc ECX
-s:  mov EAX, ECX
-    dec ECX
-    swap %1, ECX
-    ;S_OUTPUT 1, %1, ECX
-    ;mov byte [%2 + ECX], 0
-%endmacro
+    
 %macro ler 2
     mov eax, 3
     mov ebx, 0
@@ -162,6 +148,22 @@ C_INPUT: ; INPUT(* char)
     push ecx
     push edx
     ler ebp+8, 1 
+    endl
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+    leave
+    ret
+    
+C_OUTPUT: ; OUTPUT(* char) 
+    enter 0,0
+    push eax
+    push ebx
+    push ecx
+    push edx
+    escreve ebp+8, 1 
+    endl
     pop edx
     pop ecx
     pop ebx
@@ -225,6 +227,48 @@ fimfiminput:
     leave
 ret
 
+
+OUTPUT: ; OUTPUT(* int) 
+    enter 4,0
+    pusha
+    mov ECX, 0
+    mov EAX, [ebp+8]
+    mov EBX, [ebp+12]
+    mov dword [ebp-4],10
+    cmp EAX, 0
+    jge l
+    neg EAX
+l:  cdq
+    div dword [ebp-4]
+    add EDX, 0x30
+    mov byte [EBX + ECX], DL
+    inc ECX
+    ;sub EDX, EDX
+    cmp EAX, 0
+    jne l
+    ;inc ECX
+    mov EAX, [ebp+8]
+    cmp eax, 0
+    jge s
+    mov byte [EBX + ECX ], '-'
+    inc ECX
+s:  mov EAX, ECX
+    dec ECX
+    push ebx
+    push ECX
+    call swap
+    ;mov byte [%2 + ECX], 0
+    mov edx, eax
+    mov ecx, ebx
+    mov eax, 4
+    mov ebx, 1
+    int 80h
+    endl
+    popa
+    leave
+ret
+
+
 section .text
 
 global _start
@@ -232,31 +276,34 @@ _start:
     mov ebp, esp; for correct debugging
     push n
     call INPUT
-;	atoi n, a, TAMa 
-;	sub dword [n], 12
+;   atoi n, a, TAMa 
+;   sub dword [n], 12
 ;        mov eax, 1023  
  ;       mult n2
  ;       mov [n],eax
-        itoa b, n
-        S_OUTPUT 1, b, 10
-       ; S_OUTPUT 1, b, TAMa
+ push b
+ push dword [n]
+ call OUTPUT
+
+        S_OUTPUT 1, cd, 100
+        ;S_OUTPUT 1, a, TAMa
         sum1 a
         mov eax, 10
       ;  mult n2
 
-	stop
+    stop
 
 
 section .data
-	a: db "567865456"
+    a: db "12"
    ;      4294967295
-	TAMa EQU $-a
-	cd: db "isso eh um teste"
+    TAMa EQU $-a
+    cd: db "isso eh um teste", 0xa
         o:  db "overflow"
-	n2: dd 10
-;	b: db "0"
-	k: dd 329496712
+    n2: dd 10
+;   b: db "0"
+    k: dd 329496712
+        endlineeee: db 0xa
 section .bss
-	n: resd	1
-	b: resb 100
-
+    n: resd 1
+    b: resb 100
